c715731e41a2696f022ca2a182190875835faf19
Daniel Solawa <danielsolawa@gmail.com>
2018-01-20 11:59:08 +0100

    added CategoryController

---
+++ src/main/java/com/danielsolawa/springwebflux/controller/CategoryController.java
@@ -0,0 +1,39 @@
+package com.danielsolawa.springwebflux.controller;
+
+import com.danielsolawa.springwebflux.domain.Category;
+import com.danielsolawa.springwebflux.repository.CategoryRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+/**
+ * Created by Daniel Solawa on 2018-01-20.
+ */
+
+@RestController
+@RequestMapping(CategoryController.BASE_URL)
+public class CategoryController {
+
+
+    public static final String BASE_URL = "/api/v1/categories";
+
+    private final CategoryRepository categoryRepository;
+
+    public CategoryController(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    Flux<Category> getCategoryList(){
+        return categoryRepository.findAll();
+    }
+
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    Mono<Category> getCategoryById(@PathVariable String id){
+        return categoryRepository.findById(id);
+    }
+}
